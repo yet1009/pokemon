4d41e21309ab06c515f1d4b503962723fa8ddace
yet1009 <yeontae.y@ingstory.kr>
2023-08-02 10:05:48 +0900

    commits

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import MainPage from "./pages/MainPage/index.jsx";
 import DetailPage from "./pages/DetailPage/index.jsx";
+import LoginPage from "./pages/LoginPage/index.jsx";
 
 
 
@@ -12,6 +13,7 @@ const App = () => {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<MainPage />} />
+                <Route path='/login' element={<LoginPage />} />
                 <Route path='/pokemon/:id' element={<DetailPage />} />
             </Routes>
         </BrowserRouter>
